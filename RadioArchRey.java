8b2c46dba1116a32e50602c4d1cef2529ef57ebd
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2021-01-20 10:57:06 -0800

    Update RadioArchRey.java

--- RadioArchRey.java
+++ RadioArchRey.java
@@ -1,1 +1,47 @@
+public class RadioArchRey implements RadioGeneral{
 
+  int boton1 = 0;
+  int boton2 = 0;
+  int boton3 = 0;
+  int boton4 = 0;
+  int boton5 = 0;
+  int boton6 = 0;
+  int boton7 = 0;
+  int boton8 = 0;
+  int boton9 = 0;
+  int boton10 = 0;
+  int boton11 = 0;
+  int boton12 = 0;
+  boolean encendido = false;
+  String frecuencia = "AM";
+
+    
+  public boolean isOn(){
+
+  }
+
+  public void encender(){
+
+  }
+
+  public void apagar(){
+
+  }
+
+  public void incrementar(){
+
+  }
+
+  public boolean asignar(){
+
+  }
+
+  public boolean emisora(){
+
+  }
+
+  public void frecuencia(){
+
+  }
+
+}
