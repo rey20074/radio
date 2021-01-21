1d19a7fa133207620d0dae0024fa96ad0c5ddd93
Mariano Reyes <69815578+rey20074@users.noreply.github.com>
2021-01-21 08:19:10 -0600

    ultima version

--- Main.java
+++ Main.java
@@ -1,113 +1,128 @@
-/*
-Clase general main donde se implementa la radio, muy parecida para toda la clase
-
-autores: Manuel Archila y Mariano Reyes
-
-ultima modificacion: 21/01/2021
-*/
 import java.util.Scanner;
-
 public class Main{
-	
-    //metodo principal donde se encuentra el menu principal
     public static void main(String[] args){
         RadioArchRey radio=new RadioArchRey();
         Scanner scan=new Scanner(System.in);
 
         int opcion=0,opcion1=0;
-		
-		System.out.println("------------RADIO--------------");
-		
-		while (radio.isOn()==false) {
 
-			System.out.println("¿Desea encender la radio?" + "\n1. SI " + "\n2. NO");
-			opcion=scan.nextInt();
-				
-			try {
-				
-				switch(opcion) {
-				
-				case 1:
-					
-					System.out.println("Radio Encendida");
-					radio.encender();
-					break;
-					
-					
-				case 2:
-					
-					System.out.println("Ingrese una opcion...");
-					break;
-					
-					
-				default:
-					
-					System.out.println("Fuera de rango...");
-					
-					break;
+        System.out.println("------------RADIO--------------");
 
-			}
-			
-			}catch(Exception e) {
-				
-				System.out.println("Solo deben ser datos numericos");
+        while (!radio.isOn()) {
 
-				
-			}
-			
-			
-			
-		}
-		
-		while(radio.isOn()==true) {
-			
-			System.out.println("¿Que acción desea realizar? "+ "\n1. Apagar " + "\n2. Cambiar AM a FM y FM a AM "+"\n3. Guardar una Emisora" +"\n4. Elegir Emisora"+"\n5. Siguiente emisora " + "\n6. Anterior emisora ");
-			opcion1=scan.nextInt();
-			
-			try {
-				
-				switch(opcion1) {
-				
-				case 1:
-					
-					System.out.println("Radio Apagada");
-					radio.apagar();
-					break;
-				case 2:
-					
-					radio.frecuencia();
-					break;
-				case 3:
-					System.out.println("Elija el botón donde desea guardar su emisora, del 1 al 12");
-					int a=scan.nextInt();
-					radio.asignar(a);
-					break;
-				case 4:
-					System.out.println("Elija el botón a reproducir, del 1 al 12");
-					int b=scan.nextInt();
-					radio.emisoras(b);
-					break;
-				case 5:
-					radio.incrementar();
-					break;
-				case 6:
-					radio.disminuir();
-					break;
-				default:
-					System.out.println("Fuera de rango...");
-				
-				}
-				
-				
-				
-			}catch(Exception e) {
-				
-				System.out.println("Solo deben ser datos numericos");
-				
-			}
-			
-			
-		}
-		scan.close();
-	}
+            System.out.println("�Desea encender la radio?" + "\n1. SI " + "\n2. NO");
+            opcion=scan.nextInt();
+
+            try {
+
+                switch(opcion) {
+
+                    case 1:
+
+                        System.out.println("Radio Encendida");
+                        radio.encender();
+                        break;
+
+
+                    case 2:
+
+                        System.out.println("Ingrese una opcion...");
+                        break;
+
+
+                    default:
+
+                        System.out.println("Fuera de rango...");
+
+                        break;
+
+                }
+
+            }catch(Exception e) {
+
+                System.out.println("Solo deben ser datos numericos");
+
+
+            }
+
+
+
+        }
+
+        while(radio.isOn()==true) {
+
+            System.out.println("�Que acci�n desea realizar? "+ "\n1. Apagar " + "\n2. Escoger frecuencia (AM O FM) "+"\n3. Guardar una Emisora" + "\n4. Siguiente emisora " + "\n5. Escoger una emisora.");
+            opcion1=scan.nextInt();
+
+            try {
+
+                switch(opcion1) {
+
+                    case 1:
+
+                        System.out.println("Radio Apagada");
+                        radio.apagar();
+                        break;
+                    case 2:
+
+                        radio.frecuencia();
+                        break;
+                    case 3:
+                        System.out.println("A que boton desea asignar la emisora?");
+                        boolean s = true;
+                        int botoncito = 0;
+                        while(s){
+                            try{
+                                botoncito = scan.nextInt();
+                                if(botoncito<13 && botoncito>0){
+                                    s = false;
+                                }else{
+                                    System.out.println("El boton que escogio no existe");
+                                }
+                            }catch(Exception e){
+                                System.out.println("Ingrese uno de los botones validos.");
+
+                            }
+                        }
+                        radio.asignar(botoncito);
+                        break;
+                    case 4:
+                        radio.incrementar();
+                        break;
+                    case 5:
+                        System.out.println("Que boton desea presionar.");
+                        s = true;
+                        int botoneta = 0;
+                        try{
+                            while(s){
+                                botoneta = scan.nextInt();
+                                if(botoneta<13 && botoneta>0){
+                                    s =false;
+                                }else{
+                                    System.out.println("Escoja uno de los 12 botones.");
+                                }
+                            }
+                        }catch(Exception e){
+                            System.out.println("Esa opcion no es valida.");
+                        }
+                        radio.emisoras(botoneta);
+                        break;
+                    default:
+                        System.out.println("Fuera de rango...");
+
+
+
+                }
+
+
+
+            }catch(Exception e) {
+
+                System.out.println("Solo deben ser datos numericos");
+
+            }
+
+
+        }
+    }
 }
--- RadioArchRey.java
+++ RadioArchRey.java
@@ -1,272 +1,253 @@
-/*
-Clase donde se crea la radio que implementa la interfaz radio General
-
-autores: Manuel Archila y Mariano Reyes
-
-ultima modificacion: 21/01/2021
-*/
-public class RadioArchRey implements RadioGeneral{
-
-  //atributos iniciales de la clase
-  double boton1 = 0;
-  double boton2 = 0;
-  double boton3 = 0;
-  double boton4 = 0;
-  double boton5 = 0;
-  double boton6 = 0;
-  double boton7 = 0;
-  double boton8 = 0;
-  double boton9 = 0;
-  double boton10 = 0;
-  double boton11 = 0;
-  double boton12 = 0;
+public class RadioArchRey implements RadioGeneral {
+  double boton1 = 550;
+  double boton2 = 560;
+  double boton3 = 720;
+  double boton4 = 840;
+  double boton5 = 89.5;
+  double boton6 = 105.1;
+  double boton7 = 102.9;
+  double boton8 = 630;
+  double boton9 = 870;
+  double boton10 = 95.9;
+  double boton11 = 94.3;
+  double boton12 = 91.1;
   boolean encendido = false;
   private String frec = "AM";
   private double emiso = 530;
 
-    
-  //metodo sobrescrito para comprobar el estado de la radio
+
+  //test
   public boolean isOn(){
-    if(this.encendido == true){
-      return true;
-    }else{
-      return false;
-    }
+      if(this.encendido == true){
+          return true;
+      }else{
+          return false;
+      }
   }
-  
-  //metodo sobrescrito para encender la radio
+
   public void encender(){
-    encendido=true;
+      encendido=true;
   }
 
-  //metodo sobrescrito para apagar la radio
   public void apagar(){
-    encendido=false;
+      encendido=false;
   }
 
-  //metodo sobrescrito para incrementar entre emisoras
   public void incrementar(){
-    if((frec).equals("AM")){
-      emiso = emiso +10;
-      if(emiso > 1610){
-        emiso = 530;
+      if((frec).equals("AM")){
+          emiso = emiso +10;
+          if(emiso > 1610){
+              emiso = 530;
+          }
+      }else{
+          emiso = emiso + 0.2;
+          if(emiso > 107.9){
+              emiso = 87.9;
+          }
       }
-    }else{
-      emiso = emiso + 0.2;
-      if(emiso > 107.9){
-        emiso = 87.9;
-      }
-    }
-    System.out.println("Esta es la emisora actual: "+emiso);
+      System.out.println("Esta es la emisora actual: "+emiso);
   }
 
-  //metodo sobrescrito para disminuir entre emisoras
   public void disminuir(){
-    if((frec).equals("AM")){
-      emiso = emiso -10;
-      if(emiso > 530){
-        emiso = 1610;
+      if((frec).equals("AM")){
+          emiso = emiso -10;
+          if(emiso > 530){
+              emiso = 1610;
+          }
+      }else{
+          emiso = emiso - 0.2;
+          if(emiso < 87.9){
+              emiso = 107.9;
+          }
       }
-    }else{
-      emiso = emiso - 0.2;
-      if(emiso < 87.9){
-        emiso = 107.9;
-      }
-    }
-    System.out.println("Esta es la emisora actual: "+emiso);
+      System.out.println("Esta es la emisora actual: "+emiso);
   }
 
   //test para ver si le dan un 13 o no
-  //metodo sobrescrito para asignar emisoras a un boton
   public boolean asignar(int a){
-    boolean prueba=true;
+      boolean prueba=true;
 
-    switch(a){
-      case 1:
-        boton1=emiso;
-        break;
-      case 2:
-        boton2=emiso;
-        break;
-      case 3:
-        boton3=emiso;
-        break;
-      case 4:
-        boton4=emiso;
-        break;
-      case 5:
-        boton5=emiso;
-        break;
-      case 6:
-        boton6=emiso;
-        break;
-      case 7:
-        boton7=emiso;
-        break;
-      case 8:
-        boton8=emiso;
-        break;
-      case 9:
-        boton9=emiso;
-        break;
-      case 10:
-        boton10=emiso;
-        break;
-      case 11:
-        boton11=emiso;
-        break;
-      case 12:
-        boton12=emiso;
-        break;
-      default:
-        prueba=false;
-        break;
-    }
-    System.out.println("Se guardó la emisora "+emiso+" en el botón "+a);
-    return prueba;
+      switch(a){
+          case 1:
+              boton1=emiso;
+              break;
+          case 2:
+              boton2=emiso;
+              break;
+          case 3:
+              boton3=emiso;
+              break;
+          case 4:
+              boton4=emiso;
+              break;
+          case 5:
+              boton5=emiso;
+              break;
+          case 6:
+              boton6=emiso;
+              break;
+          case 7:
+              boton7=emiso;
+              break;
+          case 8:
+              boton8=emiso;
+              break;
+          case 9:
+              boton9=emiso;
+              break;
+          case 10:
+              boton10=emiso;
+              break;
+          case 11:
+              boton11=emiso;
+              break;
+          case 12:
+              boton12=emiso;
+              break;
+          default:
+              prueba=false;
+              break;
+      }
+      System.out.println("Se guardó la emisora "+emiso+" en el botón "+a);
+      return prueba;
   }
 
-  //metodo sobrescrito para poner la emisora almacenada en un boton
   public boolean emisoras(int a){
-    boolean prueba=true;
+      boolean prueba=true;
 
-    switch(a){
-      case 1:
-        emiso=boton1;
-        if(emiso<530){
-          frec="FM";
-        }
-        else{
-          frec="AM";
-        }
-        break;
-      case 2:
-        emiso=boton2;
-        if(emiso<530){
-          frec="FM";
-        }
-        else{
-          frec="AM";
-        }
-        break;
-      case 3:
-        emiso=boton3;
-        if(emiso<530){
-          frec="FM";
-        }
-        else{
-          frec="AM";
-        }
-        break;
-      case 4:
-        emiso=boton4;
-        if(emiso<530){
-          frec="FM";
-        }
-        else{
-          frec="AM";
-        }
-        break;
-      case 5:
-        emiso=boton5;
-        if(emiso<530){
-          frec="FM";
-        }
-        else{
-          frec="AM";
-        }
-        break;
-      case 6:
-        emiso=boton6;
-        if(emiso<530){
-          frec="FM";
-        }
-        else{
-          frec="AM";
-        }
-        break;
-      case 7:
-        emiso=boton7;
-        if(emiso<530){
-          frec="FM";
-        }
-        else{
-          frec="AM";
-        }
-        break;
-      case 8:
-        emiso=boton8;
-        if(emiso<530){
-          frec="FM";
-        }
-        else{
-          frec="AM";
-        }
-        break;
-      case 9:
-        emiso=boton9;
-        if(emiso<530){
-          frec="FM";
-        }
-        else{
-          frec="AM";
-        }
-        break;
-      case 10:
-        emiso=boton10;
-        if(emiso<530){
-          frec="FM";
-        }
-        else{
-          frec="AM";
-        }
-        break;
-      case 11:
-        emiso=boton11;
-        if(emiso<530){
-          frec="FM";
-        }
-        else{
-          frec="AM";
-        }
-        break;
-      case 12:
-        emiso=boton12;
-        if(emiso<530){
-          frec="FM";
-        }
-        else{
-          frec="AM";
-        }
-        break;
-        default:
-        prueba=false;
-        break;
-    }
-    System.out.println("Se esta tocando la emisora "+emiso);
-    return prueba;
+      switch(a){
+          case 1:
+              emiso=boton1;
+              if(emiso<530){
+                  frec="FM";
+              }
+              else{
+                  frec="AM";
+              }
+              break;
+          case 2:
+              emiso=boton2;
+              if(emiso<530){
+                  frec="FM";
+              }
+              else{
+                  frec="AM";
+              }
+              break;
+          case 3:
+              emiso=boton3;
+              if(emiso<530){
+                  frec="FM";
+              }
+              else{
+                  frec="AM";
+              }
+              break;
+          case 4:
+              emiso=boton4;
+              if(emiso<530){
+                  frec="FM";
+              }
+              else{
+                  frec="AM";
+              }
+              break;
+          case 5:
+              emiso=boton5;
+              if(emiso<530){
+                  frec="FM";
+              }
+              else{
+                  frec="AM";
+              }
+              break;
+          case 6:
+              emiso=boton6;
+              if(emiso<530){
+                  frec="FM";
+              }
+              else{
+                  frec="AM";
+              }
+              break;
+          case 7:
+              emiso=boton7;
+              if(emiso<530){
+                  frec="FM";
+              }
+              else{
+                  frec="AM";
+              }
+              break;
+          case 8:
+              emiso=boton8;
+              if(emiso<530){
+                  frec="FM";
+              }
+              else{
+                  frec="AM";
+              }
+              break;
+          case 9:
+              emiso=boton9;
+              if(emiso<530){
+                  frec="FM";
+              }
+              else{
+                  frec="AM";
+              }
+              break;
+          case 10:
+              emiso=boton10;
+              if(emiso<530){
+                  frec="FM";
+              }
+              else{
+                  frec="AM";
+              }
+              break;
+          case 11:
+              emiso=boton11;
+              if(emiso<530){
+                  frec="FM";
+              }
+              else{
+                  frec="AM";
+              }
+              break;
+          case 12:
+              emiso=boton12;
+              if(emiso<530){
+                  frec="FM";
+              }
+              else{
+                  frec="AM";
+              }
+              break;
+          default:
+              prueba=false;
+              break;
+      }
+      System.out.println("Se esta tocando la emisora "+emiso);
+      return prueba;
   }
 
-  //metodo sobrescrito para el cambio de frecuencias AM y FM
   public void frecuencia(){
-    if ((frec).equals("AM")){
-      frec = "FM";
-      emiso = 87.9;
-    }else {
-      frec = "AM";
-      emiso = 530;
-    }
-    System.out.println("Esta es la frecuencia actual: "+frec);
+      if ((frec).equals("AM")){
+          frec = "FM";
+          emiso = 87.9;
+      }else {
+          frec = "AM";
+          emiso = 530;
+      }
+      System.out.println("Esta es la frecuencia actual: "+frec);
   }
 
-  //getters de atributos privados
   public String getFrec(){
-    return frec;
+      return frec;
   }
 
   public double getEmiso(){
-    return emiso;
+      return emiso;
   }
-
-
 }
