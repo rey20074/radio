a28c61a2b7c37881c3a828fb63b73242cbaec154
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2021-01-22 10:29:14 -0800

    Add files via upload

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"RadioArchRey"},{"p":"<Unnamed>","l":"RadioArchReyTest"},{"p":"<Unnamed>","l":"RadioGeneral"}]
